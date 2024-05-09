72abd1f121f104c3dec70cd43b1378e113f2ad55
samanshaiza004 <samanshaiza@yahoo.com>
2024-05-09 20:06:40 +0000

    fixed drizzle db error

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
